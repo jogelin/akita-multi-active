9ce6504f7672b1cf4a7dd1648017f5c925b01d65
jogelin <gelin.jonathan@gmail.com>
2019-01-31 13:41:40 +0100

    error because of null value

--- src/app/contacts/state/contacts.service.ts
+++ src/app/contacts/state/contacts.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ContactsStore } from './contacts.store';
-import { IDS } from '@datorama/akita';
+import { ID, IDS } from '@datorama/akita';
 
 @Injectable({ providedIn: 'root' })
 export class ContactsService {
@@ -10,9 +10,8 @@ export class ContactsService {
     this.contactsStore.addActive(contactIDS);
   }
 
-  setActiveContact(contactIDS: IDS): void {
-    // TS2345: Argument of type 'IDS' is not assignable to parameter of type 'ID[]'.   Type 'string' is not assignable to type 'ID[]'.
-    // this.contactsStore.setActive(contactIDS);
+  setActiveContact(contactIDS: ID[]): void {
+    this.contactsStore.setActive(contactIDS);
   }
 
   removeActiveContact(contactIDS: IDS): void {
